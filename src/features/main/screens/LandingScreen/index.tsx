41dfdead046e1bceca5348aedd4a7e96f5550940
Marina <migova.dev@gmail.com>
2025-12-14 13:46:50 +0300

    add LandingScreenNavigationProp

--- src/features/main/screens/LandingScreen/index.tsx
+++ src/features/main/screens/LandingScreen/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
+
 import { Text, TouchableOpacity, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-import { styles } from './styles';
-import { useNavigation } from '@react-navigation/native';
-import { RootStackName } from '../../../../navigation/RootNavigator/types';
+import {LandingScreenNavigationProp, RootStackName} from '../../../../navigation/RootNavigator/types';
 
-export const LandingScreen = () => {
-  const navigation = useNavigation<any>();
+import { styles } from './styles';
+
+export const LandingScreen = ({navigation}: LandingScreenNavigationProp) => {
 
   const goToBiometric = () => {
     navigation.navigate(RootStackName.Biometric);
@@ -18,7 +18,7 @@ export const LandingScreen = () => {
       <View style={styles.content}>
         <Text style={styles.title}>Collection of little things</Text>
         <TouchableOpacity style={styles.button} onPress={goToBiometric}>
-          <Text style={styles.buttonText}>Go to Hello Screen</Text>
+          <Text style={styles.buttonText}>Go to biometrics</Text>
         </TouchableOpacity>
       </View>
     </SafeAreaView>
--- src/navigation/RootNavigator/types.ts
+++ src/navigation/RootNavigator/types.ts
@@ -1,3 +1,6 @@
+import {StackScreenProps} from "@react-navigation/stack";
+
+
 export enum RootStackName {
   Landing = 'Landing',
   Biometric = 'Biometric',
@@ -7,3 +10,5 @@ export type RootStackParamList = {
   [RootStackName.Landing]: undefined;
   [RootStackName.Biometric]: undefined;
 };
+
+export type LandingScreenNavigationProp = StackScreenProps<RootStackParamList, RootStackName.Landing>;
